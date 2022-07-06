53a5e5be6b9ac9c45bb72f3607791f86b09aee2f
Michal Převrátil <michprev@gmail.com>
2022-07-06 12:57:13 +0200

    :sparkles: Connect LSP client to external Woke LSP server

--- src/extension.ts
+++ src/extension.ts
@@ -1,11 +1,47 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import * as net from 'net';
+
+import {
+    LanguageClient,
+    LanguageClientOptions,
+    ServerOptions,
+    StreamInfo
+} from 'vscode-languageclient/node';
+
+
+let client: LanguageClient;
+
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+    const serverOptions: ServerOptions = () => {
+        let socket = net.connect({
+            port: 65432,
+            host: "127.0.0.1"
+        });
+        let result: StreamInfo = {
+            writer: socket,
+            reader: socket
+        };
+        return Promise.resolve(result);
+    }
+
+    const clientOptions: LanguageClientOptions = {
+        documentSelector: [{ scheme: 'file', language: 'solidity' }],
+        synchronize: {}
+    }
+
+    client = new LanguageClient("ABCH Tools for Solidity LSP", serverOptions, clientOptions);
+    client.start();
 }
 
 // this method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() {
+    if (!client)
+        return undefined;
+
+    return client.stop();
+}
