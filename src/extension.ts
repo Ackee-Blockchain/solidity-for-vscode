4db0266f3c421eb434b22db916a0195c87c0fcfc
Michal Převrátil <michprev@gmail.com>
2022-12-13 21:34:42 +0100

    ⬆  Bump `woke` target version

--- src/extension.ts
+++ src/extension.ts
@@ -24,7 +24,7 @@ import { ChildProcess, execFile, execFileSync } from 'child_process';
 let client: LanguageClient | undefined = undefined;
 let wokeProcess: ChildProcess | undefined = undefined;
 
-const WOKE_TARGET_VERSION = "1.3.0";
+const WOKE_TARGET_VERSION = "1.3.1";
 const WOKE_PRERELEASE = false;
 
 async function installWoke(outputChannel: vscode.OutputChannel, pythonExecutable: string): Promise<boolean> {
