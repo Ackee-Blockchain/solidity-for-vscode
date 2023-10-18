d72e44af96b68564181a44702ab712714bae342c
Stepan Sonsky <stepansonsky@gmail.com>
2023-10-18 22:37:44 +0200

    Tree structure paths, sorting, detection data, icons

--- src/detections.ts
+++ src/detections.ts
@@ -1,15 +1,24 @@
 import * as vscode from 'vscode';
-import { context } from './extension'
+import { context, log } from './extension'
 import { Command, DiagnosticSeverity, integer } from 'vscode-languageclient';
 import {
     Diagnostic
 } from 'vscode-languageclient/node';
 
+export class WakeDiagnostic extends vscode.Diagnostic{
+    data : DiagnosticData
+
+    constructor(range: vscode.Range, message: string, severity: vscode.DiagnosticSeverity, data : DiagnosticData){
+        super(range, message, severity);
+        this.data = data;
+    }
+}
+
 export class WakeDetection {
     uri: vscode.Uri;
-    diagnostic: vscode.Diagnostic;
+    diagnostic: WakeDiagnostic;
 
-    constructor(uri : vscode.Uri, diagnostic: vscode.Diagnostic){
+    constructor(uri : vscode.Uri, diagnostic: WakeDiagnostic){
         this.uri = uri;
         this.diagnostic = diagnostic;
     }
@@ -17,9 +26,21 @@ export class WakeDetection {
     getId() : string{
         return [this.uri.toString(), this.diagnostic.message, this.diagnostic.range.start.line, this.diagnostic.range.start.character, this.diagnostic.range.end.line, this.diagnostic.range.end.character].join(":");
     }
+
+    getImpact() : string | undefined{
+        log.d(this.diagnostic.data.impact);
+        return this.diagnostic.data.impact;
+    }
 }
 
-class BaseItem<T> extends vscode.TreeItem {
+export interface DiagnosticData{
+    impact : string;
+    confidence : string;
+    ignored : boolean;
+    sourceUnitName: string;
+}
+
+class BaseItem<T extends BaseItem<any>> extends vscode.TreeItem {
     originalLabel: string;
     childs: T[] = [];
     childsMap: Map<string, T> = new Map<string, T>();
@@ -29,8 +50,8 @@ class BaseItem<T> extends vscode.TreeItem {
         this.originalLabel = label
     }
 
-    addChild(id: string, item: T){
-        this.childsMap.set(id, item);
+    addChild(item: T){
+        this.childsMap.set(item.getId(), item);
         this.childs.push(item);
     }
 
@@ -38,11 +59,21 @@ class BaseItem<T> extends vscode.TreeItem {
         return this.childsMap.get(id);
     }
 
+    sortChilds(){
+        this.childs.forEach(it => {
+            it.sortChilds();
+        })
+    }
+
     clearChilds() {
         this.childsMap.clear()
         while (this.childs.length != 0) {
             this.childs.pop();
         }
+    }
+
+    getId() : string{
+        return this.originalLabel;
     }
 
     setIcon(name : string){
@@ -57,8 +88,9 @@ class BaseItem<T> extends vscode.TreeItem {
     }
 }
 
-class ImpactItem extends BaseItem<FileItem> {
+class ImpactItem extends BaseItem<PathItem> {
     impact: string;
+    detectionsCount = 0;
 
     constructor(impact: string) {
         let label: string
@@ -83,9 +115,68 @@ class ImpactItem extends BaseItem<FileItem> {
                 break;
         }
         super(label, vscode.TreeItemCollapsibleState.Expanded);
-        this.originalLabel = label
         this.impact = impact;
         this.setIcon("impact_" + impact)
+    }
+
+    addDetection(detection : WakeDetection){
+        let segments = detection.diagnostic.data.sourceUnitName.split("/");
+        let currentNode : BaseItem<ImpactItem | PathItem | FileItem> = this;
+        let fileNode : FileItem | undefined;
+
+        if(segments.length > 1){
+
+            for (let i = 0; i < segments.length-1; i++) {
+                let segment = segments[i];
+                let childNode : PathItem | FileItem | undefined = currentNode?.childsMap.get(segment);
+                if (childNode == undefined){
+                    childNode = new PathItem(segment);
+                    currentNode.addChild(childNode);
+                }
+                currentNode = childNode;
+            }
+        }
+        fileNode = currentNode.getChild(segments[segments.length - 1]) as FileItem
+        if (fileNode == undefined) {
+            fileNode = new FileItem(detection.uri);
+            currentNode.addChild(fileNode)
+        }
+        fileNode.addChild(new DetectionItem(detection));
+        this.detectionsCount++;
+    }
+
+    updateLabel() {
+        this.label = this.originalLabel + " (" + this.detectionsCount + ")";
+    }
+
+    clearChilds(): void {
+        super.clearChilds()
+        this.detectionsCount = 0;
+    }
+}
+
+class SeverityItem extends BaseItem<FileItem> {
+    severity: integer;
+
+    constructor(severity: integer) {
+        let label: string
+        switch (severity) {
+            case 1:
+                label = "Error"
+                break;
+            case 2:
+                label = "Warning"
+                break;
+            case 3:
+                label = "Info"
+                break;
+            default:
+                label = "Hide"
+                break;
+        }
+        super(label, vscode.TreeItemCollapsibleState.Expanded);
+        this.severity = severity;
+        this.setIcon("severity_" + severity)
     }
 
     updateLabel() {
@@ -97,18 +188,46 @@ class ImpactItem extends BaseItem<FileItem> {
     }
 }
 
+class PathItem extends BaseItem<PathItem | FileItem> {
+    constructor(segment : string) {
+        super(segment, vscode.TreeItemCollapsibleState.Expanded);
+        this.setIcon("folder");
+    }
+
+    addChild(item: PathItem | FileItem): void {
+        super.addChild(item)
+    }
+
+    sortChilds(): void {
+        this.childs.sort((a: PathItem | FileItem, b: PathItem | FileItem) => {
+            if (a instanceof PathItem && b instanceof FileItem){
+                return -1;
+            } else if (a instanceof FileItem && b instanceof PathItem) {
+                return 1;
+            } else {
+                return a.originalLabel.localeCompare(b.originalLabel)
+            }
+        })
+        super.sortChilds();
+    }
+}
+
 class FileItem extends BaseItem<DetectionItem> {
     uri : vscode.Uri;
 
     constructor(uri : vscode.Uri){
-        super(uri.path.substring(uri.path.lastIndexOf("/") + 1), vscode.TreeItemCollapsibleState.Collapsed);
+        super(uri.path.substring(uri.path.lastIndexOf("/") + 1), vscode.TreeItemCollapsibleState.Expanded);
         this.uri = uri;
+        this.setIcon("solidity");
     }
 
-    addChild(id: string, item: DetectionItem) {
-        super.addChild(id, item);
+    addChild(item: DetectionItem) {
+        super.addChild(item);
+    }
+
+    sortChilds(): void {
         this.childs.sort((a: DetectionItem, b: DetectionItem) => {
-            if (a.detection.diagnostic.range.start.line > b.detection.diagnostic.range.start.line){
+            if (a.detection.diagnostic.range.start.line > b.detection.diagnostic.range.start.line) {
                 return 1;
             }
             else if (a.detection.diagnostic.range.start.line < b.detection.diagnostic.range.start.line) {
@@ -132,20 +251,21 @@ class DetectionItem extends BaseItem<any> {
     detection: WakeDetection;
 
     constructor(detection: WakeDetection, collapsibleState?: vscode.TreeItemCollapsibleState) {
-        super("[Ln " + detection.diagnostic.range.start.line + "] " + detection.diagnostic.message, collapsibleState);
+        super("L" + detection.diagnostic.range.start.line + ": " + detection.diagnostic.message, collapsibleState);
         this.detection = detection;
         this.command = {
             title: "Open",
             command: "Tools-for-Solidity.detections.open_file",
             arguments: [detection.uri, detection.diagnostic.range]
         };
+        this.setIcon("detection_" + detection.diagnostic.data.impact + "_" + detection.diagnostic.data.confidence);
     }
 }
 
 export class DetectionsProvider implements vscode.TreeDataProvider<vscode.TreeItem>{
 
     outputChannel: vscode.OutputChannel;
-    detectionsMap: Map<vscode.Uri, WakeDetection[]> = new Map<vscode.Uri, WakeDetection[]>()
+    detectionsMap: Map<string, WakeDetection[]> = new Map<string, WakeDetection[]>()
     rootNodesMap: Map<string, ImpactItem> = new Map<string, ImpactItem>()
     rootNodes: ImpactItem[] = [];
 
@@ -162,7 +282,7 @@ export class DetectionsProvider implements vscode.TreeDataProvider<vscode.TreeIt
     }
 
     add(uri: vscode.Uri, detections: WakeDetection[]){
-        this.detectionsMap.set(uri, detections);
+        this.detectionsMap.set(uri.toString(), detections);
         this.refresh();
     }
 
@@ -173,39 +293,25 @@ export class DetectionsProvider implements vscode.TreeDataProvider<vscode.TreeIt
 
         for (const [key, value] of this.detectionsMap) {
             value.forEach(detection => {
-
-                let impact : string;
-                switch (detection.diagnostic.severity) {
-                    case 0:
-                        impact = "high"
-                        break;
-                    case 1:
-                        impact = "medium"
-                        break;
-                    case 2:
-                        impact = "low"
-                        break;
-                    case 3:
-                        impact = "warning"
-                        break;
-                    default:
-                        impact = "info"
-                        break;
-                }
-
-                let rootNode = this.rootNodesMap.get(impact)
+                let rootNode = this.rootNodesMap.get(detection.diagnostic.data.impact)
 
                 if (rootNode != undefined){
-                    let fileItem = rootNode.getChild(detection.uri.toString());
-                    if (fileItem == undefined){
-                        fileItem = new FileItem(detection.uri);
-                        rootNode.addChild(detection.uri.toString(), fileItem);
-                    }
-                    fileItem.addChild(detection.getId(), new DetectionItem(detection));
+                    rootNode.addDetection(detection);
+                    //let child = rootNode.getChild(detection.uri.toString());
+                    //if (fileItem == undefined){
+                    //    fileItem = new FileItem(detection.uri);
+                    //    rootNode.addChild(detection.uri.toString(), fileItem);
+                    // }
+                    // fileItem.addChild(detection.getId(), new DetectionItem(detection));
                 }
                 
             })
         }
+
+        this.rootNodes.forEach( it => {
+            it.sortChilds();
+        })
+
         this.updateRootLabels();
         this._onDidChangeTreeData.fire(undefined);
     }
@@ -213,9 +319,9 @@ export class DetectionsProvider implements vscode.TreeDataProvider<vscode.TreeIt
     updateRootLabels(){
         this.rootNodes.forEach(it => {
             it.updateLabel();
-            it.childs.forEach(it =>{
-                it.updateLabel();
-            })
+            //it.childs.forEach(it =>{
+            //   it.updateLabel();
+            //})
         })
     }
 
@@ -232,7 +338,6 @@ export class DetectionsProvider implements vscode.TreeDataProvider<vscode.TreeIt
             return item.childs;
         }
     }
-    
 }
 
 export class WakeDetectionsProvider extends DetectionsProvider {
