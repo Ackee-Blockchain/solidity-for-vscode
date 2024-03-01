a1be2b520a1d8e8d183c375f044906d0a116dd19
Stepan Sonsky <stepansonsky@gmail.com>
2024-03-01 16:45:07 +0100

    ClientErrorHandler

--- src/Analytics.ts
+++ src/Analytics.ts
@@ -56,6 +56,10 @@ export enum EventType{
     ERROR_WAKE_INSTALL_PIPX = "error_wake_install_pipx",
     ERROR_WAKE_INSTALL_PIP = "error_wake_install_pip",
     ERROR_WAKE_VERSION = "error_wake_version",
+    ERROR_WAKE_VERSION_UNKNOWN = "error_wake_version_unknown",
     ERROR_WAKE_CRASH = "error_wake_crash",
-    ERROR_WAKE_RESTART_CONNECTION = "error_wake_restart_connection"
+    ERROR_WAKE_CONNECTION_ERROR_CONTINUE = "error_wake_connection_error_continue",
+    ERROR_WAKE_CONNECTION_ERROR_SHUTDOWN = "error_wake_connection_error_shutdown",
+    ERROR_WAKE_CONNECTION_CLOSE_RESTART = "error_wake_connection_close_restart",
+    ERROR_WAKE_CONNECTION_CLOSE_DO_NOT_RESTART = "error_wake_connection_close_do_not_restart"
 }
---
+++ src/ClientErrorHandler.ts
@@ -0,0 +1,53 @@
+import {
+    ErrorHandler,
+    BaseLanguageClient,
+    Message,
+    ErrorHandlerResult,
+    ErrorAction,
+    CloseHandlerResult,
+    CloseAction
+} from 'vscode-languageclient/node';
+import { Analytics, EventType } from './Analytics';
+import { OutputChannel } from 'vscode';
+
+export class ClientErrorHandler implements ErrorHandler {
+
+    private client: BaseLanguageClient | undefined
+    private readonly restarts: number[];
+    private maxRestartCount: number = 5
+
+    constructor(private outputChannel: OutputChannel, private analytics: Analytics) {
+        this.restarts = [];
+    }
+
+    public error(_error: Error, _message: Message, count: number): ErrorHandlerResult {
+        if (count && count <= 3) {
+            this.analytics.logEvent(EventType.ERROR_WAKE_CONNECTION_ERROR_CONTINUE)
+            return { action: ErrorAction.Continue };
+        }
+        this.analytics.logEvent(EventType.ERROR_WAKE_CONNECTION_ERROR_SHUTDOWN)
+        return { action: ErrorAction.Shutdown };
+    }
+
+    public closed(): CloseHandlerResult {
+        this.restarts.push(Date.now());
+        if (this.restarts.length <= this.maxRestartCount) {
+            this.analytics.logEvent(EventType.ERROR_WAKE_CONNECTION_CLOSE_RESTART)
+            return { action: CloseAction.Restart };
+        } else {
+            const diff = this.restarts[this.restarts.length - 1] - this.restarts[0];
+            if (diff <= 3 * 60 * 1000) {
+                this.analytics.logEvent(EventType.ERROR_WAKE_CONNECTION_CLOSE_DO_NOT_RESTART)
+                return { action: CloseAction.DoNotRestart, message: `The ${this.client?.name} server crashed ${this.maxRestartCount + 1} times in the last 3 minutes. The server will not be restarted. See the output for more information.` };
+            } else {
+                this.analytics.logEvent(EventType.ERROR_WAKE_CONNECTION_CLOSE_RESTART)
+                this.restarts.shift();
+                return { action: CloseAction.Restart };
+            }
+        }
+    }
+
+    public setClient(client: BaseLanguageClient){
+        this.client = client;
+    }
+}
