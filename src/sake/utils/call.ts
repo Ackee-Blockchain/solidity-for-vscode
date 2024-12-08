c8b0b33463dc7eb7d484189546a574a540c6278f
soy rubio <70545288+soyrubio@users.noreply.github.com>
2024-12-08 11:44:34 +0100

    🐛 Fix BigInt serialization from call outputs

--- src/sake/utils/call.ts
+++ src/sake/utils/call.ts
@@ -18,11 +18,16 @@ export function decodeCallReturnValue(
     const _out: any[] = [];
     for (let i = 0; i < decoded.__length__; i++) {
         const _outputField = func.outputs[i];
+
+        // @dev BigInt values need to be converted to strings as they cannot be serialized
+        const _value =
+            typeof decoded[i] === 'bigint' ? (decoded[i] as bigint).toString() : decoded[i];
+
         _out.push({
             name: _outputField.name
                 ? `${_outputField.name} [${_outputField.internalType}]`
                 : _outputField.internalType,
-            value: decoded[i]
+            value: _value
         });
     }
 
