85d15fbe6a5a3c0df0aa9cd165843a1e35281051
soy rubio <70545288+soyrubio@users.noreply.github.com>
2024-12-11 17:32:35 +0100

    ⬆️ Up min wake version

--- src/installers/installerInterface.ts
+++ src/installers/installerInterface.ts
@@ -1,6 +1,6 @@
 import { ExecaChildProcess } from 'execa';
 
-export const WAKE_MIN_VERSION = '4.13.0';
+export const WAKE_MIN_VERSION = '4.13.2';
 
 export interface Installer {
     setup(): Promise<void>;
